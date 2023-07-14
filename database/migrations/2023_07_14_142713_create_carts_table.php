f89f86f3007cab0ca7a0e2a3686473304e0e607d
Ramly Jeson Obrero <rjc.obrero@gmail.com>
2023-07-14 23:46:22 +0800

    Scheme Update
    - Change cart user_id to nullable for logged out carts

--- database/migrations/2023_07_14_142713_create_carts_table.php
+++ database/migrations/2023_07_14_142713_create_carts_table.php
@@ -15,7 +15,7 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->integer('user_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
