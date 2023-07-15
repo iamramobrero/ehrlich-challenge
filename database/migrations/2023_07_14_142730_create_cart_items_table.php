ab8397d095949e08f4bfa6f2e10d045a96bde87f
Ramly Jeson Obrero <rjc.obrero@gmail.com>
2023-07-15 20:56:04 +0800

    Cart Item Schema Revision
    - Added price to cart_items table

--- database/migrations/2023_07_14_142730_create_cart_items_table.php
+++ database/migrations/2023_07_14_142730_create_cart_items_table.php
@@ -18,6 +18,7 @@ class CreateCartItemsTable extends Migration
             $table->integer('cart_id');
             $table->integer('product_id');
             $table->integer('quantity');
+            $table->float('price');
             $table->timestamps();
             $table->softDeletes();
         });
